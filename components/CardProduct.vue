d06631d5a8b0b8bce6e3d378bcd7ef673e995689
raihanmd <mraihanf471@gmail.com>
2023-10-25 20:26:30 +0700

    fiz responsive card product

---
+++ components/CardProduct.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RiShoppingCart2Line } from "vue-remix-icons";
+import toRupiah from "@develoka/angka-rupiah-js";
+
+const { product } = defineProps({
+  product: {
+    type: Object,
+    default: {},
+  },
+});
+</script>
+<template>
+  <NuxtLink :to="`/product/${product.productSlug}`">
+    <section class="bg-white max-w-60 sm:w-60 h-auto rounded-lg shadow-sm transition-all hover:-translate-y-1 hover:shadow-lg">
+      <img :src="product.productImage" class="w-full h-full max-h-[255px] object-fit rounded-t-lg" />
+      <div class="p-5 relative flex gap-2 flex-col justify-between h-[9em] sm:h-[10em] w-full">
+        <div class="flex flex-col gap-2">
+          <Rating v-model="product.productRating" :cancel="false" readonly unstyled class="flex gap-1" />
+          <h3 class="text-md md:text-lg font-quicksand bold whitespace-normal overflow-hidden text-ellipsis" style="display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical">{{ product.productName }}</h3>
+        </div>
+
+        <div class="flex justify-end">
+          <span class="text-md font-normal">{{ toRupiah(product.productPrice, { floatingPoint: 0 }) }}</span>
+        </div>
+
+        <div
+          class="cursor-pointer absolute z-20 -top-5 sm:-top-7 right-3 sm:right-7 w-9 h-9 sm:w-[50px] sm:h-[50px] bg-white shadow-xl rounded-full flex justify-center items-center hover:bg-yellow-300 transition duration-300 p-[6px] sm:p-3"
+        >
+          <RiShoppingCart2Line />
+        </div>
+      </div>
+    </section>
+  </NuxtLink>
+</template>
--- pages/product/index.vue
+++ pages/product/index.vue
@@ -5,22 +5,40 @@ useSeoMeta({
   description: "Product page Lynx Shop website.",
 });
 
-const { data, error, pending } = await useFetch("https://api.ecommerce.raihanmd.site/api/products");
+const { data, error, pending } = await useFetch("https://api.ecommerce.raihanmd.site/v1/product");
 </script>
 
 <template>
-  <div class="container p-2 mx-auto w-full">
+  <div class="container mx-auto w-full">
     <div class="py-10">
       <span v-if="pending">Loading...</span>
       <span v-else-if="error">Error: {{ error }}</span>
-      <div class="mb-6 flex justify-end gap-6">
-        <!-- <Dropdown @selected-category="selectedCategory = $event" /> -->
-      </div>
-      <div class="flex gap-6 flex-wrap mx-auto">
+      <!-- <div class="mb-6 flex justify-end gap-6">
+        <Dropdown @selected-category="selectedCategory = $event" />
+      </div> -->
+      <div v-if="data" class="flex gap-2 px-2 flex-wrap justify-center w-full">
         <template v-for="item in data.payload" :key="item.productId">
-          <CardsCardProduct :product="item" class="w-[calc(100%/4-18px)]" />
+          <CardProduct :product="item" class="card-product" />
         </template>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.card-product {
+  flex: none;
+}
+
+@media screen and (max-width: 600px) {
+  .card-product {
+    flex: 1 1 170px;
+  }
+}
+
+@media screen and (max-width: 640px) {
+  .card-product {
+    flex: 1 1 190px;
+  }
+}
+</style>
